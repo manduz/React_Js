d24e6a3f8ffc6eab18cfb9833daa78a35975b1b6
manduz <emekmanduz3623@gmail.com>
2024-11-14 01:29:05 +0300

    react temel yapısı

--- IlkProje/ilkprojem/src/App.js
+++ IlkProje/ilkprojem/src/App.js
@@ -1,13 +1,40 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  const students = 45000;
+  const dogruMu = true;
+  const date = 'date';
   return (
-    <div className="App">
-      <header className="App-header">
-       Emek
-      </header>
-    </div>
+   // <div>
+  //  <>
+  //     <h1>Isim Soyisim</h1>
+  //     <p>Öğrenci Sayısı: {students}</p>
+  //     <input type = "text" />
+
+  //     {dogruMu ? <p>Öğrenci Sayısı</p> : <p>Kurs Sayısı</p>}
+
+  //     <input type = {date} />
+
+
+  //   </>
+   // </div>
+
+  //  <div className = "box">
+    
+  //  </div>
+
+  //inline css kullandık
+  <div style = {{
+      width: '250px',
+      height: '250px',
+      backgroundColor: 'blue',
+      fontSize: '30px',
+  }}
+  ></div>
+
+    
   );
 }
 
