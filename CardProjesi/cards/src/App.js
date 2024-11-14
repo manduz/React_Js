58eb4af942ac58e94cdca850973307e7f50c2859
manduz <emekmanduz3623@gmail.com>
2024-11-15 01:22:17 +0300

    bulma sitesinden faydalandım.

--- CardProjesi/cards/src/App.js
+++ CardProjesi/cards/src/App.js
@@ -1,16 +1,43 @@
 
+import 'bulma/css/bulma.css'
 import './App.css';
 import  Course  from './Course';
+import Angular from './images/angular.jpg'
+import Bootstrap from './images/bootstrap5.png'
+import Kompleweb from './images/kompleweb.jpg'
+import Csharp from './images/ccsharp.png'
+
 
 function App() {
   return (
     <div className="App">
-        <Course title="Angular" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestias delectus unde, iste fugiat velit, labore qui officia fugit quam tempora nobis laborum laudantium adipisci ut nam eveniet voluptate voluptatibus."/>
-        <Course title="Bootstrap 5" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestias delectus unde, iste fugiat velit, labore qui officia fugit quam tempora nobis laborum laudantium adipisci ut nam eveniet voluptate voluptatibus."/>
-        <Course title="Komple Web" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestias delectus unde, iste fugiat velit, labore qui officia fugit quam tempora nobis laborum laudantium adipisci ut nam eveniet voluptate voluptatibus."/>
-        <Course title="React JS" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestias delectus unde, iste fugiat velit, labore qui officia fugit quam tempora nobis laborum laudantium adipisci ut nam eveniet voluptate voluptatibus."/>
-   
-         
+      <section className="hero is-primary">
+          <div className="hero-body">
+            <p className="title">Öğrendiklerim</p>
+          </div>
+      </section>
+      <div className = 'container'>
+
+        <section className='section'>
+            <div className = 'columns'>
+              
+              <div className= 'column'>
+                  <Course image={Angular} title="Angular" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestias delectus unde, iste fugiat velit, labore qui officia fugit quam tempora nobis laborum laudantium adipisci ut nam eveniet voluptate voluptatibus."/>
+              </div>
+              <div className= 'column'>
+                  <Course image={Bootstrap} title="Bootstrap 5" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestias delectus unde, iste fugiat velit, labore qui officia fugit quam tempora nobis laborum laudantium adipisci ut nam eveniet voluptate voluptatibus."/>
+              </div>
+              <div className= 'column'>
+                  <Course image={Kompleweb} title="Komple Web" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestias delectus unde, iste fugiat velit, labore qui officia fugit quam tempora nobis laborum laudantium adipisci ut nam eveniet voluptate voluptatibus."/>
+              </div>
+              <div className= 'column'>
+                  <Course image={Csharp} title="C#" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur molestias delectus unde, iste fugiat velit, labore qui officia fugit quam tempora nobis laborum laudantium adipisci ut nam eveniet voluptate voluptatibus."/>
+              </div>
+            </div>
+        </section>
+        
+       
+      </div>       
     </div>
   );
 }
--- CardProjesi/cards/src/Course.js
+++ CardProjesi/cards/src/Course.js
@@ -1,31 +1,29 @@
 import React from 'react'
 
-//propslar bir objede tutuluyor
-//  export const Course = () => {
-//    return (
-//      <div>Course Div</div>
-     
-//    )
-//  };
+ function Course({ image,title,description}) {
 
-
-//function Course(props) {
- function Course({ title,description}) {
- //   console.log(props);
-    // const title = props.title;
-    // const description = props.description;
-
-
-//    const { title,description} = props;
-
-     return (
-        <div>
-            {/* <div>{props.title}</div>
-            <div>{props.description}</div> */}
-            <div>{title}</div>
-            <div>{description}</div>
+    return(
+    <div className="card">
+    <div className="card-image">
+        <figure className="image is-4by3">
+        <img
+            src={image}
+            alt="Öğrendiklerim"/>
+        </figure>
+    </div>
+    <div className="card-content">
+        <div className="media">
+        
+        <div className="media-content">
+            <p className="title is-4">{title}</p>
         </div>
-    
+        </div>
+
+        <div className="content">
+            {description}
+        </div>
+    </div>
+    </div>
      );
    
  }
