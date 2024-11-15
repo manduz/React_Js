e1d2540336f2967985acfcc1982a59b6520a59fa
manduz <emekmanduz3623@gmail.com>
2024-11-15 23:32:37 +0300

    USeState proje1 props ekleme

---
+++ State_Projesi/stateproje/src/App.js
@@ -0,0 +1,36 @@
+
+import Course from './Course.js';
+import './App.css';
+import {useState} from 'react';
+
+function getRandomCourse()
+{
+  const courseArray = ['Angular','Bootstrap','Ccsharp','KompleWeb'];
+  return courseArray[Math.floor(Math.random()*courseArray.length)];
+}
+
+
+function App() 
+{
+
+  const [courses, setCourses] = useState([]);
+
+  const handleClick = () =>{
+    setCourses([...courses, getRandomCourse()]);
+  };
+
+  const courseList =  courses.map((courses,index) =>{
+     return  <Course key={index} courseName={courses}/>
+    });
+
+  return(
+    <div className="App">
+      <button onClick = {handleClick}> Kurs Ekle</button>
+      <div>{courseList}</div>
+      <Course />
+    </div>
+  );
+  
+}
+
+export default App;
