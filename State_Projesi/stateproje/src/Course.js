ade1744f202ae77033b2fe2c740a6d853cad6a02
manduz <emekmanduz3623@gmail.com>
2024-11-16 00:08:39 +0300

    css ekleme aşamaları ve finish

--- State_Projesi/stateproje/src/Course.js
+++ State_Projesi/stateproje/src/Course.js
@@ -2,12 +2,28 @@
 import Angular from './images/angular.jpg';
 import Bootstrap from './images/bootstrap5.png';
 import Ccsharp from './images/ccsharp.png';
-import Kompleweb from './images/kompleweb.jpg';
+import KompleWeb from './images/kompleweb.jpg';
+
+import './Course.css';
+
+const courseMap ={
+    // Angular:Angular,
+    // Bootstrap:Bootstrap,
+    // Ccsharp:Ccsharp,
+    // KompleWeb:Kompleweb
+    Angular,
+    Bootstrap,
+    Ccsharp,
+    KompleWeb,
+}
 
 function Course({courseName}) {
+   // console.log(Angular);
+   // console.log(courseName);
+    console.log(courseMap[courseName]);
     return (
-    <div>
-        <img src={Angular} alt = ""/>
+    <div className='courseDiv'>
+        <img className='image' src={courseMap[courseName]} alt = "course"/>
     </div>
     );
 }
